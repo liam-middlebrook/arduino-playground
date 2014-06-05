e8da19730bb3d9659f509cd1df8824c45970f99e
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-06-05 18:40:28 -0400

    Added comments for blink

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,17 +1,40 @@
+/*
+* First off lets declare that pin 13
+* is going to be our LED's Pin. I'm
+* doing this because on the arduino 
+* itself pin13 is hooked up onto
+* an on-board LED
+*/
 int ledPin = 13;
 
+/*
+* We start off every arduino program
+* with a setup function. This function
+* is called when the arduino first 
+* "boots up" Normally we would
+* place code that only needs to be
+* called once inside our setup()
+*/
 void setup()
 {
+  // Set the mode of our led's pin
+  // to be an output
   pinMode(ledPin, OUTPUT);
 }
 
 void loop()
 {
+  // Send a digital HIGH signal to
+  // our ledPin, telling it to turn on
   digitalWrite(ledPin, HIGH);
 
+  // Wait 500 milliseconds (.5 seconds)
   delay(500);
 
+  // Send a digital LOW signal to
+  // our ledPin, telling it to turn off
   digitalWrite(ledPin, LOW);
 
+  // Wait 500 milliseconds (.5 seconds)
   delay(500);
 }
